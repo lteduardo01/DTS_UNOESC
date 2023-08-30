e0e6482d58dc3925667d5617e6286af1281901da
lteduardo01 <odraudeodelot@Outlook.com>
2023-08-30 20:37:22 -0300

    primeirocommit

---
+++ exe01/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace vetorOrdenado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = i;
+            }
+
+            Random random = new Random();
+            for (int i = vetor.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                int temp = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temp;
+            }
+
+            Array.Sort(vetor);
+
+            Console.WriteLine("Valores ordenados:");
+            foreach (int valor in vetor)
+            {
+                Console.Write(valor + " ");
+            }
+
+            Console.WriteLine(); 
+        }
+    }
+}
