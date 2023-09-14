1ee6e3c1b7288529ea3e69a5fcdf7843ad4cd695
Eduardo <odraudeodelot@outlook.com>
2023-09-14 11:58:27 -0300

    fila_string
    fila de string desenvolvida em c#

---
+++ fila_string/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<string> filaDeStrings = new Queue<string>();
+
+        filaDeStrings.Enqueue("fila");
+        filaDeStrings.Enqueue("de");
+        filaDeStrings.Enqueue("strings");
+
+        string proximoElemento = filaDeStrings.Peek();
+        Console.WriteLine($"Próximo elemento na fila: {proximoElemento}");
+
+        Console.WriteLine("Itens na fila de strings:");
+        foreach (var item in filaDeStrings)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
